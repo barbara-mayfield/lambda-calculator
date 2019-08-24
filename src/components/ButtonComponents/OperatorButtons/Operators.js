a19197ccf348e2c938d91d96c361ebddea035240
Barbara Mayfield <bmayfield1991@gmail.com>
2019-08-23 19:56:20 -0500

    used .map to iterate over the array data and returned the button component passing props

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,20 +1,24 @@
 import React, { useState } from "react";
 
 //import any components needed
-import OperatorButton from './components/ButtonComponents/OperatorButtons/OperatorButton';
+import OperatorButton from './OperatorButton';
 
 //Import your array data to from the provided data file
 import { operators } from '../../../data';
 
-const Operators = () => {
+const Operators = (props) => {
   // STEP 2 - add the imported data to state
   const [operatorState, setOperatorState] = useState(operators);
+  
 
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+       {operatorState.map(operators => {
+         return <OperatorButton key={operators} text={operators} addNumber={props.addOperator} />;
+       })}
     </div>
   );
 };
