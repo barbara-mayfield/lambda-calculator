1e064d6211037f4f0e2e7ebfc435df8eba595412
Barbara Mayfield <bmayfield1991@gmail.com>
2019-08-26 11:32:29 -0500

    cleaned up the code under step 3 in Numbers.js, added the addNumber onClick function to the NumberButton.js

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,14 +1,12 @@
 import React from "react";
 
-const NumberButton = props => {
+const NumberButton = (props) => {
   return (
-    <>
       <button
         className="number-button"
         onClick={() => props.addNumber(props.text)}>
         {props.text}
       </button>
-    </>
   );
 };
 
