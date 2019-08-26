ad42f8a9e65a2b1eded3718cd0c16387b037cf5b
Barbara Mayfield <bmayfield1991@gmail.com>
2019-08-26 11:30:27 -0500

    added number prop to Display, wrote a function to hold data in state

--- src/App.js
+++ src/App.js
@@ -16,7 +16,11 @@ function App(props) {
   // Your functions should accept a parameter of the the item data being displayed to the DOM (ie - should recieve 5 if the user clicks on
   // the "5" button, or the operator if they click one of those buttons) and then call your setter function to update state.
   // Don't forget to pass the functions (and any additional data needed) to the components as props
-  
+  const [displayValue, setDisplayValue] = useState("0");
+
+  const addNumber = (number) => {
+    setDisplayValue(displayValue => displayValue + number);
+  }
 
 
   return (
@@ -25,14 +29,13 @@ function App(props) {
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
         <div className="disp">
-        <Display />
+        <Display number={displayValue} />
         </div>
-
 
         <div className="calc">
           <Specials />
           <div className="nums">
-            <Numbers className="nums" />
+            <Numbers addNumber={addNumber} />
           </div>
         </div>
 
