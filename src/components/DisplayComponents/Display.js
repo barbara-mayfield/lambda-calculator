031986911adedb4132cddc175e84db5a3ee930e3
Barbara Mayfield <bmayfield1991@gmail.com>
2019-08-26 11:33:16 -0500

    cleaned up the code to make it easier to read in Dispaly.js

--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -1,9 +1,10 @@
 import React from "react";
 
 const Display = (props) => {
-  return <div>{/* Display any props data here */props.number}
-    
-  </div>;
+  return <div className="display">
+          {/* Display any props data here */}
+          {props.number}
+        </div>;
 };
 
 export default Display;
